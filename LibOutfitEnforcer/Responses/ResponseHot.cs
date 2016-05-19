02dd86f4a72621cb760ad67d5fc1721a50b597af
rohitph <rohitph@hotmail.com>
2016-05-19 17:30:48 -0400

    applying central failure response

--- LibOutfitEnforcer/Responses/ResponseHot.cs
+++ LibOutfitEnforcer/Responses/ResponseHot.cs
@@ -12,9 +12,9 @@ namespace LibOutfitEnforcer.Responses
         {
             CmdResponses.Add(new CommandResponse(Commands.PutOnFootwear, "sandals"));
             CmdResponses.Add(new CommandResponse(Commands.PutOnHeadwear, "sun visor"));
-            CmdResponses.Add(new CommandResponse(Commands.PutOnSocks, "fail"));
+            CmdResponses.Add(new CommandResponse(Commands.PutOnSocks, base.FAILURE_RESPONSE));
             CmdResponses.Add(new CommandResponse(Commands.PutOnShirt, "t-shirt"));
-            CmdResponses.Add(new CommandResponse(Commands.PutOnJacket, "fail"));
+            CmdResponses.Add(new CommandResponse(Commands.PutOnJacket, base.FAILURE_RESPONSE));
             CmdResponses.Add(new CommandResponse(Commands.PutOnPants, "shorts"));
             CmdResponses.Add(new CommandResponse(Commands.LeaveHouse, "leaving house"));
             CmdResponses.Add(new CommandResponse(Commands.TakeOffPajamas, "Removing PJs"));
